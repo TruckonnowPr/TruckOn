c8c6744043459705ba1bd5ba22c3e0519ccff13d
Рома <@DESKTOP-BH9GPT6>
2020-05-12 13:42:59 +0300

    Подписаны макеты для трейлера Four-door truck with double chassis and open frame;

--- BaceModel/ModelInspertionDriver/Truck/FourDoorTruckChassisOpenFrame.cs
+++ BaceModel/ModelInspertionDriver/Truck/FourDoorTruckChassisOpenFrame.cs
@@ -4,7 +4,7 @@ namespace BaceModel.ModelInspertionDriver.Truck
 {
     public class FourDoorTruckChassisOpenFrame : ITransportVehicle
     {
-        public int CountPhoto { get; set; } = 21;
+        public int CountPhoto { get; set; } = 19;
         public string Type { get; set; } = "FourDoorTruckChassisOpenFrame";
         public bool IsNextInspection { get; set; } = true;
         public List<string> NamePatern { get; set; }
@@ -17,27 +17,25 @@ namespace BaceModel.ModelInspertionDriver.Truck
         {
             NamePatern = new List<string>()
             {
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "10",
-                "11",
-                "12",
-                "13",
-                "14",
-                "15",
-                "16",
-                "17",
-                "18",
-                "19",
-                "20",
-                "21",
+                "Truck windshield",
+                "Right front headlight of a truck",
+                "Truck engine radiator",
+                "Left front headlight of a truck",
+                "The front of the truck on the passenger side",
+                "Front wheel of the truck on the passenger side",
+                "Front wheel of the truck on the passenger side (Tread)",
+                "Front door of the truck on the passenger side",
+                "Rear door of the truck on the passenger side",
+                "Rear wheel of the truck on the passenger side",
+                "Rear wheel of the truck on the passenger side (Tread)",
+                "The whole back of the truck",
+                "Rear wheel of the truck on the driver's side",
+                "Rear wheel of the truck on the driver's side (Tread)",
+                "Rear door of the truck on the driver's side",
+                "Front door of the truck on the driver's side",
+                "The front of the truck on the driver's side",
+                "Front wheel of the truck on the driver's side (Tread)",
+                "Front wheel of the truck on the driver's side",
             };
         }
     }
