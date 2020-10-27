1821cd0fe8ff4aae5560fc54dc3a75b5b3d58253
Roma <chupryna.r.v.dev@gmail.com>
2020-10-27 20:59:43 +0200

    Добалена новая верстка для страниц Assigned, Pickedup, Delivered, Billed, Paid, Archived, Deleted; Адаптация страницы  New Assigned, Pickedup, Delivered, Billed, Paid, Archived, Deletedпод наш бекенд;

--- WebDispacher/wwwroot/js/orders.js
+++ WebDispacher/wwwroot/js/orders.js
@@ -18,3 +18,35 @@ function SelectAssining(idOrderAndIdDriver, baseUrl) {
         window.location.reload();
     }
 }
+
+function SelectAssining1(idOrderAndIdDriver, baseUrl) {
+    var statusResponse;
+    if (idOrderAndIdDriver.split(',')[1] == "Unassign") {
+        let url = baseUrl+"/Dashbord/Unassign";
+        let xmlHttp = new XMLHttpRequest();
+        let idOrder = idOrderAndIdDriver.split(',')[0];
+        var body = 'idOrder=' + encodeURIComponent(idOrder);
+        xmlHttp.open("POST", url, false);
+        xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xmlHttp.setRequestHeader('Accept-Encoding', 'br');
+        xmlHttp.send(body);
+        statusResponse = xmlHttp.responseText;
+    }
+    else if (idOrderAndIdDriver != "") {
+        let idOrder = idOrderAndIdDriver.split(',')[0];
+        let idDriver = idOrderAndIdDriver.split(',')[1];
+        let url = "@ViewBag.BaseUrl/Dashbord/Assign";
+        let xmlHttp = new XMLHttpRequest();
+        var body = 'idOrder=' + encodeURIComponent(idOrder) +
+            '&idDriver=' + encodeURIComponent(idDriver);
+        xmlHttp.open("POST", url, false);
+        xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xmlHttp.setRequestHeader('Accept-Encoding', 'br');
+        xmlHttp.send(body);
+        statusResponse = xmlHttp.responseText;
+    }
+
+    if (statusResponse == "True") {
+        window.location.reload();
+    }
+}
