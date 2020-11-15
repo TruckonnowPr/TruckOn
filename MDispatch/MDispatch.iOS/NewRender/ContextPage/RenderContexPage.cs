d83349a75c1848251581581ad9ab214811a63696
The Roma <Chupryna.r.v.dev@gmail.com>
2020-11-16 01:06:12 +0200

    Оптимизация стилей;

---
+++ MDispatch/MDispatch.iOS/NewRender/ContextPage/RenderContexPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreGraphics;
+using MDispatch.iOS.NewRender.ContextPage;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(ContentPage), typeof(RenderContexPage))]
+namespace MDispatch.iOS.NewRender.ContextPage
+{
+    public class RenderContexPage : PageRenderer
+    {
+        public RenderContexPage()
+        {
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            if (NavigationController != null)
+            {
+                UIViewController vc = NavigationController.TopViewController;
+                List<UIBarButtonItem> items = new List<UIBarButtonItem>();
+                foreach (UIBarButtonItem item in vc.NavigationItem.RightBarButtonItems)
+                {
+                    if (item.Image != null)
+                    {
+                        UIButton btn = new UIButton(UIButtonType.Custom);
+                        btn.SetImage(item.Image, UIControlState.Normal);
+
+                        btn.Frame = new CGRect(0, 0, 23, 23);
+                        btn.TouchUpInside += (sender, args) =>
+                        {
+                            item.Target.PerformSelector(item.Action, null, 0.0);
+                        };
+
+                        UIBarButtonItem customItem = new UIBarButtonItem(btn);
+                        customItem.CustomView.WidthAnchor.ConstraintEqualTo(23).Active = true;
+                        customItem.CustomView.HeightAnchor.ConstraintEqualTo(23).Active = true;
+                        items.Add(customItem);
+
+                    }
+                }
+                if (items.Count != 0)
+                {
+                    vc.NavigationItem.RightBarButtonItems = items.ToArray();
+                }
+
+            }
+        }
+    }
+}
---
+++ MDispatch/MDispatch.iOS/NewRender/ContextPage/RenderPageNavigation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreGraphics;
+using FormsControls.Base;
+using MDispatch.iOS.NewRender.ContextPage;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(AnimationNavigationPage), typeof(RenderPageNavigation))]
+namespace MDispatch.iOS.NewRender.ContextPage
+{
+    public class RenderPageNavigation : NavigationRenderer
+    {
+        public RenderPageNavigation()
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var nb = (AnimationNavigationPage)Element;
+            if (InteractivePopGestureRecognizer != null)
+            {
+                InteractivePopGestureRecognizer.Enabled = false;
+
+                if (nb != null)
+                {
+                    nb.BarBackgroundColor = Color.FromHex("#ffff");
+                    nb.BarTextColor = Color.FromHex("#000000");
+                }
+
+            }
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
+            {
+                Font = UIFont.FromName("OpenSans-Bold", 20),
+            });
+        }
+    }
+}
