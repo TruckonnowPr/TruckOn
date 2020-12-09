ee78f1fb02758f02530141b9f22e834919f9c1fe
The Roma <Chupryna.r.v.dev@gmail.com>
2020-12-09 17:56:03 +0200

    Реализован новый анимированый лоадер по дизайну;

--- MDispatch/MDispatch/View/LoadPage.xaml.cs
+++ MDispatch/MDispatch/View/LoadPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Rg.Plugins.Popup.Pages;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MDispatch.View
@@ -9,11 +12,26 @@ namespace MDispatch.View
 		public LoadPage ()
 		{
 			InitializeComponent ();
-		}
+            StartAnimation();
+        }
 
         protected override bool OnBackgroundClicked()
         {
             return false;
+        }
+
+        private void StartAnimation()
+        {
+            var animation = new Animation(v => loaderImg.Scale = v, 1, 1.7);
+            animation.Commit(this, "ScaleIt", length: 400, easing: Easing.SinInOut,
+                finished: (v, c) => EndAnimation());
+        }
+
+        private void EndAnimation()
+        {
+            var animation = new Animation(v => loaderImg.Scale = v, 1.7, 1);
+            animation.Commit(this, "ScaleIt", length: 400, easing: Easing.SinInOut,
+                finished: (v, c) => StartAnimation());
         }
     }
 }
