ce4ed0efc36df3faf422d0e8115c06bb3a8d9443
The Roma <Chupryna.r.v.dev@gmail.com>
2021-01-24 14:33:01 +0200

    Добалены шрифты для андроида;

--- MDispatch/MDispatch.Android/NewrRender/Label/CorectLabel.cs
+++ MDispatch/MDispatch.Android/NewrRender/Label/CorectLabel.cs
@@ -14,12 +14,12 @@ namespace MDispatch.Droid.NewrRender.Label
 
         protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Label> e)
         {
-            base.OnElementChanged(e);
-            var element = this.Element as Xamarin.Forms.Label;
-            if (element != null && element.Text != null && element.Text != "")
-            {
-                this.Element.Text = this.Element.Text.ToUpper();
-            }
+            //base.OnElementChanged(e);
+            //var element = this.Element as Xamarin.Forms.Label;
+            //if (element != null && element.Text != null && element.Text != "")
+            //{
+            //    this.Element.Text = this.Element.Text.ToUpper();
+            //}
         }
     }
 }
