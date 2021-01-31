94ea6c68ac7ee3c7e132d1e4acbdced3c5e135e2
Roma <chupryna.r.v.dev@gmail.com>
2021-01-31 13:17:30 +0200

    Поправлен алгоритм реасаина водителя;

--- WebDispacher/wwwroot/js/orders.js
+++ WebDispacher/wwwroot/js/orders.js
@@ -10,7 +10,7 @@ function SelectAssining(idOrderAndIdDriver, baseUrl) {
             '&idDriver=' + encodeURIComponent(idDriver);
         xmlHttp.open("POST", url, false);
         xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xmlHttp.setRequestHeader('Accept-Encoding', 'br');
+        //xmlHttp.setRequestHeader('Accept-Encoding', 'br');
         xmlHttp.send(body);
         statusResponse = xmlHttp.responseText;
     }
@@ -20,33 +20,33 @@ function SelectAssining(idOrderAndIdDriver, baseUrl) {
 }
 
 function SelectAssining1(idOrderAndIdDriver, baseUrl) {
-    var statusResponse;
+    let xmlHttp = new XMLHttpRequest();
+    var body;
     if (idOrderAndIdDriver.split(',')[1] == "Unassign") {
         let url = baseUrl+"/Dashbord/Unassign";
-        let xmlHttp = new XMLHttpRequest();
         let idOrder = idOrderAndIdDriver.split(',')[0];
-        var body = 'idOrder=' + encodeURIComponent(idOrder);
-        xmlHttp.open("POST", url, false);
+        body = 'idOrder=' + encodeURIComponent(idOrder);
+        xmlHttp.open("POST", url, true);
         xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xmlHttp.setRequestHeader('Accept-Encoding', 'br');
-        xmlHttp.send(body);
-        statusResponse = xmlHttp.responseText;
     }
     else if (idOrderAndIdDriver != "") {
         let idOrder = idOrderAndIdDriver.split(',')[0];
         let idDriver = idOrderAndIdDriver.split(',')[1];
-        let url = "@ViewBag.BaseUrl/Dashbord/Assign";
-        let xmlHttp = new XMLHttpRequest();
-        var body = 'idOrder=' + encodeURIComponent(idOrder) +
+        let url = baseUrl+"/Dashbord/Assign";
+        body = 'idOrder=' + encodeURIComponent(idOrder) +
             '&idDriver=' + encodeURIComponent(idDriver);
-        xmlHttp.open("POST", url, false);
+        xmlHttp.open("POST", url, true);
         xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xmlHttp.setRequestHeader('Accept-Encoding', 'br');
-        xmlHttp.send(body);
-        statusResponse = xmlHttp.responseText;
     }
-
-    if (statusResponse == "True") {
-        window.location.reload();
-    }
+    xmlHttp.onload = function (e) {
+        if (xmlHttp.readyState === 4) {
+            if (xmlHttp.responseText == "True") {
+                window.location.reload();
+            }
+        }
+    };
+    xmlHttp.send(body);
+   
 }
